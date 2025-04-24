e2b652c25e3c284768178e2cabc7c99bcf515d0f
MrDevil-07 <132101504+Exdevilex@users.noreply.github.com>
2025-04-24 12:40:05 +0600

    Create logo.py

---
+++ logo.py
@@ -0,0 +1,55 @@
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━      PRESS ENTER TO BACK MAIN MENU
+
+
+
+#coding by : SK SIDDIK KHAN
+#coding language : python
+#github : Exdevilex
+import os
+os.system("pkg install toilet")
+os.system("clear")
+line=f"{44*'━'}"
+banner=f"""\t╻  ┏━┓┏━╸┏━┓   ┏┳┓┏━┓╻┏ ┏━╸┏━┓
+\t┃  ┃ ┃┃╺┓┃ ┃   ┃┃┃┣━┫┣┻┓┣╸ ┣┳┛
+\t┗━╸┗━┛┗━┛┗━┛   ╹ ╹╹ ╹╹ ╹┗━╸╹┗╸
+{line}
+《■》 DEVELOPER •>>> SK SIDDIK KHAN
+《■》 TELEGRAM  •>>> t.me/busy1here
+《■》 TOOLTYPE  •>>> VIRAL 2 LOGO MAKER
+{line}"""
+def main():
+ print(banner)
+ print("《1》 VIRAL STYLES LOGO 1")
+ print("《2》 VIRAL STYLES LOGO 2")
+ print(line)
+ _logo_=input(f"《■》 CHOICE •>>> ")
+ if _logo_ in ["1"]:
+  future()
+ if _logo_ in ["2"]:
+  smblok()
+def future():
+ os.system("clear")
+ print(banner)
+ print("《■》 EXAMPLE •>>> SK SIDDIK | GOJO | YOUE NAME .etc")
+ print(line)
+ nam=input("《■》INPUT YOUE NAME •>>> ")
+ print(line)
+ os.system(f"toilet -f future {nam}")
+ print(line)
+ sami=input("PRESS ENTER TO BACK MAIN MENU")
+ os.system("clear")
+ main()
+def smblok():
+ os.system("clear")
+ print(banner)
+ print("《■》EXAMPLE •>>> SK SIDDIK | GOJO | YOUE NAME .etc")
+ print(line)
+ nam=input("《■》INPUT YOUE NAME •>>> ")
+ print(line)
+ os.system(f"toilet -f smblock {nam}")
+ print(line)
+ sami=input("PRESS ENTER TO BACK MAIN MENU")
+ os.system("clear")
+ main()
+main()
